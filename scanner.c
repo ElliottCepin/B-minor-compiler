8c961fa61ccc49e31454f57b7ca80f9b2e7b3875
ElliottCepin <elliottcepin5@gmail.com>
2025-05-18 08:39:12 -0700

    scanner tokenizes identifiers (and keywords as identifiers...)

--- scanner.c
+++ scanner.c
@@ -1,24 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef enum tk{
+	IDENTIFIER,
+	NONE
+} TokenType;
+
 
 FILE *arg_read(int, char **);
+FILE *file_write(char *);
+char *type_string(TokenType);
+int valid_identifier(char, int);
 
 int error_code = 0;
 
-
 int main(int argc, char **argv){
-	int counter = 0; // this will be used to count the characters in the file
 	char next;
+	TokenType token_type = NONE;
+	char *out_file = malloc(strlen(argv[1]) + 4);
+	strcat(out_file, *argv);
+	strcat(out_file, ".tk");
 
 	FILE *file = arg_read(argc, argv);
+	FILE *out  = file_write(out_file);
 
 	while ((next = fgetc(file)) != EOF){
-		counter++;	
+		if (token_type == NONE){
+			if (valid_identifier(next, 1)){
+				token_type = IDENTIFIER;
+				fprintf(out, "%s:%c", type_string(token_type), next); // no newline; this is only part of the identifier
+				continue;
+			}
+			// TODO: expand out all cases
+			continue;
+		}
+		switch (token_type){
+			// TODO: replace token_types with enums
+			case IDENTIFIER:
+				
+				if (valid_identifier(next, 0)){
+					fprintf(out, "%c", next);
+				} else {
+					token_type = NONE;
+					fprintf(out, "\n");
+					ungetc(next, file);
+				}
+				break;
+			// TODO: expand out all cases
+			default:
+				token_type = NONE;
+				ungetc(next, file);
+				// this probably shouldn't happen once the code is fully done
+
+		}
+	}
+	fclose(out);
+	fclose(file);
+	return error_code;
+}
+
+
+// turns enum into a string for easy printing... 
+// TODO: add all enums
+char *type_string(TokenType tk){
+	switch (tk){
+		case IDENTIFIER:
+			return "IDENTIFIER";
+		default:
+			return ""; // again, this shouldn't really show up
+	}
+}
+
+// returns true if the given character could be part of an identifier
+// the first_char of an identifier cannot be a number 
+int valid_identifier(char c, int first_char){
+	if (isalpha(c) || c == '_'){
+		return 1;
 	}
 
-	printf("There are %d characters\n", counter);
+	if (!first_char && isdigit(c)){
+		return 1;
+	}
 
-	return error_code;
+	return 0;
 }
 
 FILE *arg_read(int argc, char **argv){
@@ -31,6 +97,16 @@ FILE *arg_read(int argc, char **argv){
 
 	if (file == NULL){
 		fprintf(stderr, "File: '%s' could not be opened", argv[1]);
+		exit(1);
+	}
+
+	return file;
+}
+
+FILE *file_write(char *name){
+	FILE *file = fopen(name, "w");
+	if (file == NULL){
+		fprintf(stderr, "could not open file '%s'", name);
 		exit(1);
 	}
 
