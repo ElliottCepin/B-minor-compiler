709d2230bc8bf39ff4873eafefa31071b57050f1
ElliottCepin <elliottcepin5@gmail.com>
2025-05-19 11:14:34 -0700

    Added TokenType COMMENT to scanner.c

--- scanner.c
+++ scanner.c
@@ -6,6 +6,7 @@
 typedef enum tk{
 	IDENTIFIER,
 	NUMBER,
+	COMMENT,
 	NONE
 } TokenType;
 
@@ -15,10 +16,12 @@ FILE *file_write(char *);
 char *type_string(TokenType);
 int valid_identifier(char, int);
 int valid_number(char);
-
+int valid_comment(char, int, FILE *);
 int error_code = 0;
 
 int main(int argc, char **argv){
+	int comment_type = 0; // C-style comments are 2 and C++ style commnets are 1. i.e. single line // == 2 multiline /* == 1; no comment is 0
+
 	char next;
 	TokenType token_type = NONE;
 	char *out_file = malloc(strlen(argv[1]) + 4);
@@ -40,6 +43,14 @@ int main(int argc, char **argv){
 				token_type = NUMBER;
 				fprintf(out, "%s:%c", type_string(token_type), next);
 				continue;
+			} if (valid_comment(next, comment_type, file)){
+				// it looks like we'll have to have a running "comment_type"
+				// valid_comment will peak and ungetc no matter what
+				token_type = COMMENT;
+				fprintf(out, "%s:%c", type_string(token_type), next);
+				char comment_chr2 = fgetc(file);
+				comment_type = (comment_chr2 == '/') + 1; // this will be used later
+				fprintf(out, "%c", comment_chr2);
 			}
 			// TODO: expand out all cases
 			continue;
@@ -58,11 +69,20 @@ int main(int argc, char **argv){
 					fprintf(out, "%c", next);
 					break;
 				}
+			case COMMENT:
+				if (valid_comment(next, comment_type, file)){
+					if (next == '\n' && comment_type == 1) {
+						break; // this prevents unwanted newlines in our file
+					}
+					fprintf(out, "%c", next);
+					break;
+				}
 			// TODO: expand out all cases
 			default:
 				token_type = NONE;
 				fprintf(out, "\n");
 				ungetc(next, file);
+				comment_type = 0;
 
 		}
 	}
@@ -70,9 +90,6 @@ int main(int argc, char **argv){
 	fclose(file);
 	return error_code;
 }
-
-
-
 
 // turns enum into a string for easy printing... 
 // TODO: add all enums
@@ -83,9 +100,45 @@ char *type_string(TokenType tk){
 
 		case NUMBER:
 			return "NUMBER";
+
+		case COMMENT:
+			return "COMMENT";
 		default:
 			return ""; // again, this shouldn't really show up
 	}
+}
+/*
+ * this is just here so that I can actually test that this works on comments
+ * this type of comment gets treated differently, because the switch case ungets on false return value
+ * so it has to do a get as a countermeasure, 
+ * since we want to be at the end of the comment so we don't reprocess the / in comment end
+ * also, these get flattened into one line in the .tk file
+ */
+int valid_comment(char c, int comment_type, FILE *file){
+	char next;
+
+	if (!comment_type){
+		next = fgetc(file);
+		ungetc(next, file);
+		return c == '/' && (next == '/' || next == '*');
+	}
+	
+	if (comment_type == 1){
+		next = fgetc(file);
+		if (c == '*' && next == '/'){
+			fgetc(file); // this is to circumvent my lazy switch case code
+			return 0;
+		}
+		ungetc(next, file);
+		return 1;
+	}
+	
+	if (comment_type == 2){
+		return c != '\n';
+	}
+
+
+	return 0;
 }
 
 int valid_number(char c){
