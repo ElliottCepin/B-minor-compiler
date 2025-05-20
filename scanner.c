e3168484acaefdb036fd9c006b594b5f95553fe7
ElliottCepin <elliottcepin5@gmail.com>
2025-05-20 08:20:28 -0700

    Added TokenType STRING and got escape characters working for STRING

--- scanner.c
+++ scanner.c
@@ -1,3 +1,9 @@
+// Program: scanner.c
+// Author: Elliott Cepin
+// Purpose: Creates a .tk file from a given .bm file (.bm being B-minor)
+//          the .tk holds tokens in order seperated by a newline
+//          this scanner/lexer does not classify syntax errors
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +13,7 @@ typedef enum tk{
 	IDENTIFIER,
 	NUMBER,
 	COMMENT,
+	STRING,
 	NONE
 } TokenType;
 
@@ -17,6 +24,8 @@ char *type_string(TokenType);
 int valid_identifier(char, int);
 int valid_number(char);
 int valid_comment(char, int, FILE *);
+int valid_string(char, int, FILE *, FILE *);
+
 int error_code = 0;
 
 int main(int argc, char **argv){
@@ -24,14 +33,18 @@ int main(int argc, char **argv){
 
 	char next;
 	TokenType token_type = NONE;
+	FILE *in = arg_read(argc, argv); 
+	
+	// arg_read does error checking for 
 	char *out_file = malloc(strlen(argv[1]) + 4);
-	strcat(out_file, *argv);
+	strcat(out_file, argv[1]);
 	strcat(out_file, ".tk");
-
-	FILE *file = arg_read(argc, argv);
 	FILE *out  = file_write(out_file);
-
-	while ((next = fgetc(file)) != EOF){
+	
+	int last_was_escape = 0;
+	while ((next = fgetc(in)) != EOF){
+		last_was_escape = (next == '\\') || last_was_escape;
+		
 		if (token_type == NONE){
 			if (valid_identifier(next, 1)){
 				token_type = IDENTIFIER;
@@ -43,18 +56,29 @@ int main(int argc, char **argv){
 				token_type = NUMBER;
 				fprintf(out, "%s:%c", type_string(token_type), next);
 				continue;
-			} if (valid_comment(next, comment_type, file)){
+			}
+
+			if (valid_comment(next, comment_type, in)){
 				// it looks like we'll have to have a running "comment_type"
 				// valid_comment will peak and ungetc no matter what
 				token_type = COMMENT;
 				fprintf(out, "%s:%c", type_string(token_type), next);
-				char comment_chr2 = fgetc(file);
+				char comment_chr2 = fgetc(in);
 				comment_type = (comment_chr2 == '/') + 1; // this will be used later
 				fprintf(out, "%c", comment_chr2);
+				continue;
+			}
+			
+			if (valid_string(next, 1, in, out) && !last_was_escape){
+				token_type = STRING;
+				fprintf(out, "%s:%c", type_string(token_type), next);
+				continue;
 			}
 			// TODO: expand out all cases
 			continue;
 		}
+
+		last_was_escape = ('\\' == next);
 		switch (token_type){
 			// TODO: replace token_types with enums
 			case IDENTIFIER:
@@ -70,24 +94,39 @@ int main(int argc, char **argv){
 					break;
 				}
 			case COMMENT:
-				if (valid_comment(next, comment_type, file)){
+				if (valid_comment(next, comment_type, in)){
 					if (next == '\n' && comment_type == 1) {
 						break; // this prevents unwanted newlines in our file
 					}
 					fprintf(out, "%c", next);
 					break;
 				}
+			case STRING:
+				if (valid_string(next, 0, in, out) && token_type == STRING){
+					if (next == '\\'){
+						break; // we dealt with this already
+					}
+					fprintf(out, "%c", next);
+					break;
+				}	
+				// I almost just decided that newline meant end-of-string
+				if ((next == '\"') && token_type == STRING){
+					fprintf(out, "\"");
+					next = fgetc(in);
+				}
+
+
 			// TODO: expand out all cases
 			default:
 				token_type = NONE;
 				fprintf(out, "\n");
-				ungetc(next, file);
+				ungetc(next, in);
 				comment_type = 0;
 
 		}
 	}
 	fclose(out);
-	fclose(file);
+	fclose(in);
 	return error_code;
 }
 
@@ -97,15 +136,36 @@ char *type_string(TokenType tk){
 	switch (tk){
 		case IDENTIFIER:
 			return "IDENTIFIER";
-
 		case NUMBER:
 			return "NUMBER";
 
 		case COMMENT:
 			return "COMMENT";
+		case STRING:
+			return "STRING";
 		default:
 			return ""; // again, this shouldn't really show up
 	}
+}
+
+// doesn't do anything about errors
+int valid_string(char c, int first_char, FILE *in, FILE *out){
+	if (first_char){
+		return c == '\"';
+	}
+
+	if (c == '\\'){
+		fprintf(out, "\\");
+		char next = fgetc(in);
+		if (next == '\"'){
+			fprintf(out, "\"");
+			return 1;
+		}
+		ungetc(next, in);
+		return 1;
+	}
+	
+	return (c != '\"' && c != '\n');
 }
 /*
  * this is just here so that I can actually test that this works on comments
@@ -114,22 +174,22 @@ char *type_string(TokenType tk){
  * since we want to be at the end of the comment so we don't reprocess the / in comment end
  * also, these get flattened into one line in the .tk file
  */
-int valid_comment(char c, int comment_type, FILE *file){
+int valid_comment(char c, int comment_type, FILE *in){
 	char next;
 
 	if (!comment_type){
-		next = fgetc(file);
-		ungetc(next, file);
+		next = fgetc(in);
+		ungetc(next, in);
 		return c == '/' && (next == '/' || next == '*');
 	}
 	
 	if (comment_type == 1){
-		next = fgetc(file);
+		next = fgetc(in);
 		if (c == '*' && next == '/'){
-			fgetc(file); // this is to circumvent my lazy switch case code
+			fgetc(in); // this is to circumvent my lazy switch case code
 			return 0;
 		}
-		ungetc(next, file);
+		ungetc(next, in);
 		return 1;
 	}
 	
